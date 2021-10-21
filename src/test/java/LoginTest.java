aea27881c4fd53966869ee44089ca777219c2d57
Inga <korona1504@gmail.com>
2021-10-21 13:23:43 +0300

    HW4_RenataFinkel

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,23 +1,14 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-public class LoginTest {
-    WebDriver wd;
 
-    @BeforeMethod
-    public void init(){
-        wd=new ChromeDriver();
-        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
-    }
+
+public class LoginTest extends TestBase{
 
     @Test
-    public void loginPositiveTest(){
+    public void loginPositiveTest() throws InterruptedException {
 
         //open login/Reg form
         WebElement loginBtn = wd.findElement(By.xpath("//*[text()='LOGIN']"));
@@ -34,16 +25,25 @@ public class LoginTest {
         passwordInput.clear();
         passwordInput.sendKeys("Nnoa12345$");
 
-        //click  Login button
+        //click Login button
         wd.findElement(By.xpath("//button[1]")).click();
+
         //Assert if button Logout is
+        //pause(5000);
 
         Assert.assertTrue(wd.findElements(By.xpath("//button[text()='Sign Out']")).size()>0);
-
     }
 
-    @AfterMethod
-    public void tearDown(){
-        //wd.quit();
+    @Test
+    public void loginTest2(){
+        String email = "noa@gmail.com";
+        String password = "Nnoa12345$";
+
+        openLoginRegistrationForm();
+        fillLoginRegistrationForm(email,password);
+        submitLogin();
+        pause(5000);
+
+        Assert.assertTrue(isElementPresent(By.xpath("//button[text()='Sign Out']")));
     }
 }
